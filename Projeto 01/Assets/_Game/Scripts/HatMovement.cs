0843abc8ce31d7b6918c69a6932ef42c6feee588
magalercio <magalercio.gui@gmail.com>
2023-08-03 13:29:43 -0300

    Movimento do chapeu
    Criado o Script para movimentar o chapeu na tela.
    Adcionado comentarios no script contendo informação dos comandos para que possamos lembrar suas funcionalidades e motivos no futuro.

---
+++ Projeto 01/Assets/_Game/Scripts/HatMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatMovement : MonoBehaviour
+{
+    // Serialize field faz com que a variavel mesmo privada apareça no Inspecter do Unity.
+    [SerializeField] private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DragTouch();
+    }
+
+    private void DragTouch()
+    {
+        //Verificando se o toque na tela é maior que zero E toque na tela é do tipo movimento
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //passando a posição do dedo na tela
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            //passamos o movimento para o eixo X, passando o valor float 0 para eixo Y e Z.
+            //Time.deltaTime normaliza o movimento independente da taxa de frames de um aparelho para o outro.
+            transform.Translate(touchDeltaPosition.x * speed * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
